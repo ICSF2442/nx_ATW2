a717c1047a5a20ea531ed70d74c78f95ee6150dd
ICSF2442 <icsf2442@gmail.com>
2023-06-12 19:27:04 +0100

    Register e login page feitas, algumas alterações para fazer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { UserComponent } from './views/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',loadChildren:()=>import('./views/login/login.module').then(m=>m.LoginModule),component:LoginComponent},
+  {path:'register',loadChildren:()=>import('./views/register/register.module').then(m=>m.RegisterModule),component:RegisterComponent},
+  {path:'user',loadChildren:()=>import('./views/user/user.module').then(m=>m.UserModule),component:UserComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  constructor(private router:Router){
+  }
+  goToLogin(){
+    this.router.navigate(['/login']);
+  }
+
+}
